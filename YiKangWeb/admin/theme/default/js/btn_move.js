2bcc90580d4d1cfbe21ebacbb80ccae4b41142ac
huangchiwei <659590719@qq.com>
2014-11-05 21:29:29 +0800

    sss

---
+++ YiKangWeb/admin/theme/default/js/btn_move.js
@@ -0,0 +1,17 @@
+// JavaScript 按钮图片背景样式
+
+function switchSysBar(){ 
+var locate=location.href.replace('center.html','');
+var ssrc=document.all("img1").src.replace(locate,'');
+
+if (ssrc.indexOf("theme/default/images/main_18.gif")>=0)
+{ 
+document.all("img1").src="theme/default/images/main_18_1.gif";
+document.all("frmTitle").style.display="none" 
+} 
+else
+{ 
+document.all("img1").src="theme/default/images/main_18.gif";
+document.all("frmTitle").style.display="" 
+} 
+}
